2d6c3d6f582c02af750942b840d7d663f27fc5df
Muhammad Idrees <midrees097@gmail.com>
2023-04-16 17:29:29 +0500

    implemented pre_gen brick

--- bricks/dart_package/hooks/pre_gen.dart
+++ bricks/dart_package/hooks/pre_gen.dart
@@ -1,5 +1,30 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add pre-generation logic.
+void run(HookContext context) async {
+  final logger = context.logger;
+
+  final projectName = context.vars['project_name'] as String;
+  final description = context.vars['description'] as String;
+  final publishable = context.vars['publishable'] as bool;
+
+  final process = await Process.run(
+    'mason',
+    [
+      'make',
+      'very_good_dart_package',
+      '--description',
+      description,
+      'project_name=$projectName',
+      'publishable=$publishable',
+    ],
+  );
+
+  if (process.exitCode != 0) {
+    logger.err(process.stderr);
+    exit(1);
+  }
+
+  logger.info(process.stdout);
 }
